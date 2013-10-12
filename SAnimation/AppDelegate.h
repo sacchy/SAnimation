f3d69db089583b499d54ad8741eaddd7e2ff201f
sacchy <systemtelescope@gmail.com>
2013-10-12 19:13:07 +0900

    initial commit

---
+++ SAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SAnimation
+//
+//  Created by Sacchy on 2013/10/11.
+//  Copyright (c) 2013年 Sacchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SAnimation/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SAnimation
+//
+//  Created by Sacchy on 2013/10/11.
+//  Copyright (c) 2013年 Sacchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
